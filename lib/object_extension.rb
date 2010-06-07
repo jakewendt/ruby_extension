57001009dc28f2eaa6e37ba4a3b5130980ca31a6
Jake <github@jake.otherinbox.com>
2010-06-07 10:02:38 -0700

    Redefined true? and added false?

--- lib/object_extension.rb
+++ lib/object_extension.rb
@@ -13,8 +13,14 @@ module ObjectExtension
 	module InstanceMethods
 
 		def to_boolean
-			return [true, 'true', 1, '1', 't'].include?(
+#			return [true, 'true', 1, '1', 't'].include?(
+			return ![nil, false, 'false', 0, '0', 'f'].include?(
 				( self.class == String ) ? self.downcase : self )
+		end
+		alias_method :true?, :to_boolean
+
+		def false?
+			!self.true?
 		end
 
 	end
