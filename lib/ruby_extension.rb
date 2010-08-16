0ce79d3993d5f70ba35e61acaddf022fbb7fee22
Jake <github@jake.otherinbox.com>
2010-08-16 13:22:27 -0700

    Moved files into the correct places.

---
+++ lib/ruby_extension.rb
@@ -0,0 +1,8 @@
+#	I need to be more explicit about which files to require
+#	because I have files of the same name in RAILS_ROOT/lib
+require "ruby_extension/object"
+require "ruby_extension/integer"
+require "ruby_extension/array"
+require "ruby_extension/string"
+require "ruby_extension/hash"
+require "ruby_extension/nil_class"
